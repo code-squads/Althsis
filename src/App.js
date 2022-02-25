3452cea38d4c0c05995abca07f8652a8d5b5567d
Rupesh-2003 <rupeshraut9939@gmail.com>
2022-02-26 00:46:37 +0530

    Card section completed

--- src/App.js
+++ src/App.js
@@ -5,16 +5,8 @@ import {
   Navbar, 
   RightContainer
 } from './App.styled';
-import {
-  Card,
-  CardContainer,
-  CardFlex1,
-  CardFlex2,
-  CardFlex3,
-  CardFlex4,
-  Balance,
-} from './RightContainer.styled';
 import AccountsOverview from './components/AccountsOverview';
+import Cards from './components/Cards.js';
 import { 
   FlexContainer1, 
   FlexContainer2 
@@ -36,28 +28,7 @@ const App = () => {
           </FlexContainer2>
         </LeftContainer>
         <RightContainer>
-          <CardContainer>
-            <Card>
-              <CardFlex1>
-                <img style={{marginRight: "10px"}} src='./chip.svg'></img>
-                Axis Bank
-              </CardFlex1>
-              <CardFlex2>
-                **** **** **** 1234
-              </CardFlex2>
-              <CardFlex3>
-                AccountHolder Name
-              </CardFlex3>
-              <CardFlex4>
-                Rupesh Raut 
-                <Balance>₹ 87,302.00</Balance>
-              </CardFlex4>
-            </Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-          </CardContainer>
+          <Cards></Cards>
         </RightContainer>
       </MainContainer>
     </>
