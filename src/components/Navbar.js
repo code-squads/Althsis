a683020e262ad0cb2c33cab7b443cf47aafac48a
Omkar Phansopkar <omkarphansopkar@gmail.com>
2022-02-27 13:42:23 +0530

    Replaced dummy data with placeholder data
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,6 @@ import { NavbarStyled, Profile } from "./Navbar.styled";
 import ThemeToggle from "./ThemeToggle";
 
 import { initialsFromName } from "../utils/data";
-import { data } from "../constants/dummyData";
 import AlthsisIcon from "../assets/icons/analysis.png";
 
 const Navbar = () => {
@@ -17,10 +16,7 @@ const Navbar = () => {
       <div className="float-end m-1 mx-3 d-flex align-items-center justify-content-center">
         <ThemeToggle />
         <Profile>
-          <div>{initialsFromName(data.profile.name)}</div>
-          {/* <div className='profileName'>
-            { data.profile.name }
-          </div> */}
+          <div>{initialsFromName("John Doe")}</div>
         </Profile>
       </div>
     </NavbarStyled>
